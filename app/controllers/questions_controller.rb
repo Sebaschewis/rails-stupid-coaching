2a2a30fcbfaf7ac088f6ce8e04e8aea9a94cb639
sebastian flores <saflchewis@live.com>
2024-11-25 13:48:45 -0500

    antes de bootstrap

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
-    
+
+  end
+
+  def answer
+    @answer = params[:answer]
+    if @answer == "I am going to work"
+      @response = "Great"
+    elsif @answer && @answer.to_s.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I dont care, get dressed and go to work!"
+    end
   end
 end
